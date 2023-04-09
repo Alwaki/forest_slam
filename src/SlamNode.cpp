18bf3c622882f7187ba26586e7fa9c8252fff9a9
alwaki <xalwa999@gmail.com>
2023-04-09 20:17:03 +0200

    added livox callback

--- src/SlamNode.cpp
+++ src/SlamNode.cpp
@@ -17,24 +17,59 @@ SlamNode::~SlamNode() = default;
 void SlamNode::_init_node()
 {
     // Setup subscribers and publishers
-    _lidar_livox_sub         = _nh.subscribe<livox_ros_driver::CustomMsg>
+    _lidar_livox_sub   = _nh.subscribe<livox_ros_driver::CustomMsg>
                                 ("/livox/lidar", 10, &SlamNode::_lidar_livox_callback,
                                  this, ros::TransportHints().tcpNoDelay(true));
 
-    _lidar_ouster_sub        = _nh.subscribe<sensor_msgs::PointCloud2>
+    _lidar_ouster_sub  = _nh.subscribe<sensor_msgs::PointCloud2>
                                 ("/os_cloud_node/points", 10, &SlamNode::_lidar_ouster_callback,
                                  this, ros::TransportHints().tcpNoDelay(true));
 
-    _imu_vectornav_sub       = _nh.subscribe<sensor_msgs::Imu>
+    _imu_vectornav_sub = _nh.subscribe<sensor_msgs::Imu>
                                 ("/vectornav/IMU", 10, &SlamNode::_imu_vectornav_callback,
                                  this, ros::TransportHints().tcpNoDelay(true));
 
-    _imu_ouster_sub          = _nh.subscribe<sensor_msgs::Imu>
+    _imu_ouster_sub    = _nh.subscribe<sensor_msgs::Imu>
                                 ("/os_cloud_node/imu", 10, &SlamNode::_imu_ouster_callback,
                                  this, ros::TransportHints().tcpNoDelay(true));
+
+    _lidar_livox_pub   = _nh.advertise<sensor_msgs::PointCloud2>("/livox/lidar_pc2", 10);
 }
 
-void SlamNode::_lidar_livox_callback(const livox_ros_driver::CustomMsg::ConstPtr &msgIn){}
+void SlamNode::_lidar_livox_callback(const livox_ros_driver::CustomMsg::ConstPtr &msgIn)
+{
+    
+    // Lock mutex for multi-thread
+    _lidar_livox_mtx.lock();
+
+    // Change cloud to PCL structure and push to queue
+    size_t cloudsize = msgIn->points.size();
+    CloudXYZIT cloud;
+    cloud.points.resize(cloudsize);
+
+    for (size_t i = 0; i < cloudsize; i++)
+        {
+            auto &src = msgIn->points[i];
+            auto &dst = cloud.points[i];
+            dst.x = src.x;
+            dst.y = src.y;
+            dst.z = src.z;
+            dst.intensity = src.reflectivity;
+            dst.time = src.offset_time;
+        }
+
+    // Publish corrected cloud
+    sensor_msgs::PointCloud2 tempCloud;
+    pcl::toROSMsg(cloud, tempCloud);
+    tempCloud.header.stamp = ros::Time(msgIn->timebase);
+    tempCloud.header.frame_id = "world";
+    _lidar_livox_pub.publish(tempCloud);
+
+    // Unlock mutex for multi-thread
+    _lidar_livox_mtx.unlock();
+    
+            
+}
 
 void SlamNode::_lidar_ouster_callback(const sensor_msgs::PointCloud2::ConstPtr &msgIn){}
 
