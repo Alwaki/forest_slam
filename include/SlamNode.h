1e0748542ecb4c25f0ab3315d3b3ac5f48b7eef0
alwaki <xalwa999@gmail.com>
2023-04-10 17:41:34 +0200

    Removed livox support

--- include/SlamNode.h
+++ include/SlamNode.h
@@ -14,7 +14,6 @@
 #include "sensor_msgs/PointCloud2.h"
 #include "geometry_msgs/PointStamped.h"
 #include "geometry_msgs/PoseStamped.h"
-#include "livox_ros_driver/CustomMsg.h"
 
 #include <deque>
 #include <thread>
@@ -47,15 +46,12 @@ class SlamNode
     private:
         ros::NodeHandle         _nh;
         ros::Subscriber         _lidar_ouster_sub;
-        ros::Subscriber         _lidar_livox_sub;
         ros::Subscriber         _imu_vectornav_sub;
         ros::Subscriber         _imu_ouster_sub;
-        ros::Publisher          _lidar_livox_pub;
 
-        std::mutex              _lidar_livox_mtx;
+        std::mutex              _buffer_mtx;
 
         void _init_node();
-        void _lidar_livox_callback(const livox_ros_driver::CustomMsg::ConstPtr &msgIn);
         void _lidar_ouster_callback(const sensor_msgs::PointCloud2::ConstPtr &msgIn);
         void _imu_ouster_callback(const sensor_msgs::Imu::ConstPtr &msgIn);
         void _imu_vectornav_callback(const sensor_msgs::Imu::ConstPtr &msgIn);
