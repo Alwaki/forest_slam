e43c3617a9326583cc00bcb3a94f44e68a4c9d5a
alwaki <xalwa999@gmail.com>
2023-06-14 21:38:17 +0200

    additional work

--- include/SlamNode.h
+++ include/SlamNode.h
@@ -6,53 +6,123 @@
 
 #pragma once
 
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2_ros/static_transform_broadcaster.h>
+#include <tf2_ros/buffer.h>
+#include <tf2/buffer_core.h>
+#include <tf/transform_datatypes.h>
+#include <tf/LinearMath/Quaternion.h>
+#include <tf/transform_listener.h>
+#include <tf/transform_broadcaster.h>
+
 #include <ros/ros.h>
 #include <pcl/point_cloud.h>
+#include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
 #include <pcl_conversions/pcl_conversions.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/registration/icp.h>
 #include "sensor_msgs/Imu.h"
 #include "sensor_msgs/PointCloud2.h"
 #include "geometry_msgs/PointStamped.h"
 #include "geometry_msgs/PoseStamped.h"
+#include "geometry_msgs/Point.h"
+#include "geometry_msgs/Quaternion.h"
+#include "geometry_msgs/TransformStamped.h"
+
+/*
+#include "gtsam/geometry/Pose2.h"
+#include "gtsam/geometry/Point2.h"
+#include "gtsam/inference/Symbol.h"
+#include "gtsam/slam/PriorFactor.h"
+#include "gtsam/slam/BetweenFactor.h"
+#include "gtsam/sam/BearingRangeFactor.h"
+#include "gtsam/nonlinear/NonlinearFactorGraph.h"
+#include "gtsam/nonlinear/LevenbergMarquardtOptimizer.h"
+#include "gtsam/nonlinear/Marginals.h"
+#include "gtsam/nonlinear/Values.h"
+#include "iostream"
+#include "cmath"
+#include "Eigen/Dense"
+#include "random"
+*/
 
 #include <deque>
 #include <thread>
 #include <string>
 #include <stdio.h>
 
-struct EIGEN_ALIGN16 PointXYZIT
+struct PointXYZT
+{
+    PCL_ADD_POINT4D;
+    std::uint32_t time;
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+} EIGEN_ALIGN16;
+
+POINT_CLOUD_REGISTER_POINT_STRUCT(PointXYZT,
+                                 (float, x, x) (float, y, y) (float, z, z)
+                                 (std::uint32_t, time, time))
+
+struct PointXYZIT
 {
     PCL_ADD_POINT4D;
     PCL_ADD_INTENSITY;
-    std::uint32_t time;
+    float t;
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-};
+} EIGEN_ALIGN16;
 
 POINT_CLOUD_REGISTER_POINT_STRUCT(PointXYZIT,
                                  (float, x, x) (float, y, y) (float, z, z)
-                                 (float, intensity, intensity) 
-                                 (std::uint32_t, time, time))
-
-typedef pcl::PointCloud<PointXYZIT>       CloudXYZIT;
-typedef pcl::PointCloud<PointXYZIT>::Ptr  CloudXYZITPtr;
-
+                                 (float, intensity, intensity) (float, t, t))
 
 class SlamNode
 {
     public:
-       SlamNode();
-       virtual ~SlamNode();
+        SlamNode();
+        virtual ~SlamNode();
 
     private:
-        ros::NodeHandle         _nh;
-        ros::Subscriber         _lidar_ouster_sub;
-        ros::Subscriber         _imu_vectornav_sub;
-        ros::Subscriber         _imu_ouster_sub;
+        ros::NodeHandle                 _nh;
+        ros::Subscriber                 _lidar_ouster_sub;
+        ros::Subscriber                 _imu_vectornav_sub;
+        ros::Subscriber                 _imu_ouster_sub;
+        ros::Subscriber                 _icp_odom_sub;
+        ros::Subscriber                 _dynamic_transformer_sub;
+        ros::Publisher                  _lidar_ouster_filtered_pub;
+        ros::Publisher                  _icp_odom_pub;
 
-        std::mutex              _buffer_mtx;
+
+        std::mutex                              _buffer_mtx;
+        pcl::PointCloud<pcl::PointXYZ>::Ptr     _prev_cloud;
+        bool                                    _prev_cloud_flag;
+
+        pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> _icp;
+
+        geometry_msgs::Point                    _total_odom;
+        geometry_msgs::Quaternion               _current_rot;
+
+        tf::TransformListener                   _tf_listener;
+        tf2_ros::TransformBroadcaster           _dynamic_broadcaster;
+
+        /*
+        gtsam::NonlinearFactorGraph     _graph; 
+        std::vector<gtsam::Symbol>      _pose_symbols; 
+        std::vector<gtsam::Symbol>      _landmark_symbols;
+        */
 
         void _init_node();
         void _lidar_ouster_callback(const sensor_msgs::PointCloud2::ConstPtr &msgIn);
+        void _lidar_odom_calculate(const sensor_msgs::PointCloud2::ConstPtr &msgIn);
+        void _dynamic_transformer(const geometry_msgs::PointStamped::ConstPtr &msgIn);
+        void _lidar_feature_extraction(const pcl::PointCloud<PointXYZIT>::ConstPtr &msgIn);
         void _imu_ouster_callback(const sensor_msgs::Imu::ConstPtr &msgIn);
         void _imu_vectornav_callback(const sensor_msgs::Imu::ConstPtr &msgIn);
+
+        /*
+        void _add_landmark(const gtsam::Symbol::ConstPtr &landmarkMsg);
+        void _add_pose(const gtsam::Symbol::ConstPtr &poseMsg);
+        void _optimize();
+        void _get_map();
+        */
 };
--- src/SlamNode.cpp
+++ src/SlamNode.cpp
@@ -5,7 +5,6 @@
  */
 
 #include "SlamNode.h"
-//#include "GraphOptimizer.h"
 //#include "FeatureEstimator.h"
 
 SlamNode::SlamNode():
@@ -16,6 +15,18 @@ SlamNode::~SlamNode() = default;
 
 void SlamNode::_init_node()
 {
+    // Set paramaters
+    _icp.setMaxCorrespondenceDistance (0.05);
+    _icp.setMaximumIterations (50);
+    _icp.setTransformationEpsilon (1e-8);
+    _icp.setEuclideanFitnessEpsilon (1);
+
+    // Initialize necessary variables
+    _prev_cloud_flag = 0;
+    _total_odom.x = 0;
+    _total_odom.y = 0;
+    _total_odom.z = 0;
+
     // Setup subscribers and publishers
     _lidar_ouster_sub  = _nh.subscribe<sensor_msgs::PointCloud2>
                                 ("/os_cloud_node/points", 10, &SlamNode::_lidar_ouster_callback,
@@ -29,16 +40,131 @@ void SlamNode::_init_node()
                                 ("/os_cloud_node/imu", 10, &SlamNode::_imu_ouster_callback,
                                  this, ros::TransportHints().tcpNoDelay(true));
 
+    _icp_odom_sub  = _nh.subscribe<sensor_msgs::PointCloud2>
+                                ("/os_cloud_node/points", 10, &SlamNode::_lidar_odom_calculate,
+                                 this, ros::TransportHints().tcpNoDelay(true));
+
+    _dynamic_transformer_sub  = _nh.subscribe<geometry_msgs::PointStamped>
+                                ("/icp/transform", 10, &SlamNode::_dynamic_transformer,
+                                 this, ros::TransportHints().tcpNoDelay(true));
+
+    _lidar_ouster_filtered_pub = _nh.advertise<sensor_msgs::PointCloud2>("/os_cloud_node/points_filtered", 10);
+
+    _icp_odom_pub = _nh.advertise<geometry_msgs::PointStamped>("/icp/transform", 10);
+
 }
 
 void SlamNode::_lidar_ouster_callback(const sensor_msgs::PointCloud2::ConstPtr &msgIn)
 {
-    ROS_INFO("hi");
+    /*
+    1. Trim the point cloud height wise and distance wise (or somehow segment ground away)
+    2. Publish trimmed point cloud for feature extraction and lidar odometry calculation
+    */
+
+    // Change to PCL format
+    pcl::PCLPointCloud2::Ptr pcl_cloud (new pcl::PCLPointCloud2 ());
+    pcl_conversions::toPCL(*msgIn, *pcl_cloud);
+
+    // Downsample cloud
+    pcl::PCLPointCloud2::Ptr pcl_voxel (new pcl::PCLPointCloud2 ());
+    pcl::VoxelGrid<pcl::PCLPointCloud2> voxel_grid;
+    voxel_grid.setInputCloud(pcl_cloud);
+    voxel_grid.setLeafSize (0.15f, 0.15f, 0.15f);
+    voxel_grid.filter (*pcl_voxel);
+
+    // Passthrough filter
+    pcl::PCLPointCloud2::Ptr pcl_pass (new pcl::PCLPointCloud2 ());
+    pcl::PassThrough<pcl::PCLPointCloud2> pass;
+    pass.setInputCloud (pcl_voxel);
+    pass.setFilterFieldName ("z");
+    pass.setFilterLimits (-2.0, 2.5);
+    pass.filter (*pcl_pass);
+
+    // Publish cloud
+    _lidar_ouster_filtered_pub.publish(*pcl_pass);
+
+
 }
 
-void SlamNode::_imu_ouster_callback(const sensor_msgs::Imu::ConstPtr &msgIn){}
+void SlamNode::_lidar_odom_calculate(const sensor_msgs::PointCloud2::ConstPtr &msgIn)
+{
+    // Downsample cloud and convert to ICP compatible format
+    pcl::PCLPointCloud2::Ptr pcl_cloud (new pcl::PCLPointCloud2 ());
+    pcl::PCLPointCloud2::Ptr pcl_voxel (new pcl::PCLPointCloud2 ());
+    pcl_conversions::toPCL(*msgIn, *pcl_cloud);
+    pcl::VoxelGrid<pcl::PCLPointCloud2> voxel_grid;
+    voxel_grid.setInputCloud(pcl_cloud);
+    voxel_grid.setLeafSize (0.2f, 0.2f, 0.2f);
+    voxel_grid.filter (*pcl_voxel);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr icp_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::fromPCLPointCloud2(*pcl_voxel,*icp_cloud);
 
-void SlamNode::_imu_vectornav_callback(const sensor_msgs::Imu::ConstPtr &msgIn){}
+    if(_prev_cloud_flag == 1)
+    {
+        pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
+        icp.setMaxCorrespondenceDistance (0.05);
+        icp.setMaximumIterations (50);
+        icp.setTransformationEpsilon (1e-8);
+        icp.setEuclideanFitnessEpsilon (1);
+        icp.setInputSource(icp_cloud);
+        icp.setInputTarget(_prev_cloud);
+        pcl::PointCloud<pcl::PointXYZ>::Ptr aligned_cloud (new pcl::PointCloud<pcl::PointXYZ>);
+        icp.align(*aligned_cloud);
+        Eigen::Matrix4d transformation_matrix = icp.getFinalTransformation().cast<double>();
+        geometry_msgs::PointStamped odom;
+        odom.header.stamp = msgIn->header.stamp;
+        odom.header.frame_id = msgIn->header.frame_id;
+        odom.point.x = transformation_matrix(0,3);
+        odom.point.y = transformation_matrix(1,3);
+        odom.point.z = transformation_matrix(2,3);
+        _icp_odom_pub.publish(odom);
+    }
+    
+    _prev_cloud = icp_cloud;
+    _prev_cloud_flag = 1;
+}
+
+void SlamNode::_dynamic_transformer(const geometry_msgs::PointStamped::ConstPtr &msgIn)
+{
+    geometry_msgs::TransformStamped transform;
+    transform.transform.rotation = _current_rot;
+    _total_odom.x += msgIn->point.x;
+    _total_odom.y += msgIn->point.y;
+    _total_odom.z += msgIn->point.z;
+
+    // Set translation transform of drone from POS message
+    transform.transform.translation.x = _total_odom.x;
+    transform.transform.translation.y = _total_odom.y;
+    transform.transform.translation.z = _total_odom.z;
+
+    // Publish pose transform of drone
+    transform.header.stamp = msgIn->header.stamp;
+    transform.child_frame_id = "base_link";
+    transform.header.frame_id = "map";
+    _dynamic_broadcaster.sendTransform(transform);
+}
+
+void SlamNode::_lidar_feature_extraction(const pcl::PointCloud<PointXYZIT>::ConstPtr &msgIn)
+{
+    /*
+    1. Extract landmark features from trimmed point cloud
+    2. Pass these landmarks to optimizer
+    */
+}
+
+
+void SlamNode::_imu_ouster_callback(const sensor_msgs::Imu::ConstPtr &msgIn)
+{
+    /*
+    1. 
+    
+    */
+}
+
+void SlamNode::_imu_vectornav_callback(const sensor_msgs::Imu::ConstPtr &msgIn)
+{
+    _current_rot = msgIn->orientation;
+}
 
 int main(int argc, char **argv)
 {
